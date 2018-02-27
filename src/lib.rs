12265ebddaaf1b95592cd09f7d772336b8828e8a
Scott Teal <scott@teals.org>
2018-02-26 23:44:02 -0500

    Moved crypto primatives into sub-section.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod multi_crypto;
 
 enum TriState {
     Yes,
@@ -35,44 +36,6 @@ enum EntryFormat {
     /// contents encrypted, prefixed with ID, and signed
     EncryptedThenIdSigned,
 };
-
-
-/// A format encoding a cryptographic hash
-struct MultiHash {
-    code: u32,
-    size: u32,
-    hash: Vec<u8>
-}
-
-/// A format encoding a cryptographic key
-struct MultiKey {
-    code: u32,
-    size: u32,
-    key_type: u32,
-    crypto_type: u32,
-    crypto_scheme: u32,
-    key: Vec<u8>
-}
-
-/// A format encoding the prelude for an encrypted block
-struct MultiEncrypt {
-    code: u32,
-    size: u32,
-    side_channel: Vec<u8>,
-}
-
-/// A format encoding a cryptographic signature
-struct MultiSignature {
-    code: u32,
-    size: u32,
-    side_channel: Vec<u8>,
-}
-impl MultiSignature {
-    /// Determine if the signature is valid for a given key
-    pub fn is_signed_by(key: MultiKey) -> bool {
-        false
-    }
-}
 
 enum EntryType {
     /// File: fixed # of entries, ordered unique entry IDs
@@ -185,12 +148,4 @@ struct Entry {
     /// Signature status
     meta_is_signed: bool,
     meta_signature: MultiSignature,
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
